b0e11a6949e9943f71ed1a00c2936adc070f837a
jholbertcaro <jholbert.c@gmail.com>
2021-07-28 11:52:50 -0400

    Nuevo diseño login¨

--- src/components/auth/Login.vue
+++ src/components/auth/Login.vue
@@ -1,124 +1,144 @@
 <template>
-  <v-container fill-height>
-    <v-row justify="center" align="center">
-      <ValidationObserver ref="obs" v-slot="{ passes }">
-        <v-col cols="12" md="12">
-          <v-img
-            class="mx-auto"
-            width="60%"
-            contain
-            src="@/assets/img/edifier-logo-color.svg"
-          ></v-img>
-          <v-card
-            flat
-            class="px-0 py-0"
-            style="
-              box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1) !important;
-              border-radius: 20px !important;
-            "
-          >
-            <v-col cols="12" md="12">
-              <label for="email">Correo Electrónico</label>
-              <ValidationProvider
-                name="Nombre"
-                rules="email|required"
-                v-slot="{ errors }"
-              >
-                <v-text-field
-                  v-model="email"
-                  class="mt-2"
-                  color="#00A0E9"
-                  dense
-                  outlined
-                  placeholder="ejemplo@mail.com"
-                  :error-messages="errors"
-                ></v-text-field>
-              </ValidationProvider>
-            </v-col>
-            <v-col cols="12" md="12" class="mt-n5">
-              <label for="email">Contraseña</label>
-              <ValidationProvider
-                name="Contraseña"
-                rules="required"
-                v-slot="{ errors }"
-              >
-                <v-text-field
-                  @keyup.enter="passes(HandlerLogin)"
-                  type="password"
-                  v-model="password"
-                  class="mt-2"
-                  color="#00A0E9"
-                  dense
-                  outlined
-                  placeholder="Ingresa tu contraseña"
-                  :error-messages="errors"
-                ></v-text-field>
-              </ValidationProvider>
-            </v-col>
-            <v-col cols="12" md="12">
-              <!-- <div class="d-md-flex justify-md-space-between"> -->
-              <!-- <div class="d-flex justify-center"> -->
-              <v-btn
-                :loading="loading"
-                @click="passes(HandlerLogin)"
-                tile
-                elevation="0"
-                large
-                block
-                dark
-                color="#00A0E9"
-                class="text-capitalize"
-              >
-                Ingresar
-              </v-btn>
-              <v-btn
-                :loading="loading"
-                @click="HandlerRouter('signup')"
-                tile
-                elevation="0"
-                large
-                block
-                dark
-                color="black"
-                class="text-capitalize mt-1"
-              >
-                Registrar
-              </v-btn>
-              <!-- </div> -->
-              <!-- </div> -->
-            </v-col>
-            <v-col class="text-center mt-n7 mt-md-n5" cols="12" sm="12" md="12">
-              <div
-                @click="HandlerRouter('login')"
-                style="cursor: pointer"
-                class="mt-6 blue--text"
-              >
-                Recuperar contraseña
+  <v-container fluid class="mx-0 px-0 py-0 my-0">
+    <v-row justify="center" align="center" no-gutters>
+      <v-col cols="12" md="5">
+        <div
+          style="width: 100%; height: 100vh; background-color: #ebf1f7"
+          class="d-flex flex-column"
+        >
+          <div>
+            <div class="py-15"></div>
+            <v-img
+              class="mx-auto"
+              contain
+              width="250"
+              src="@/assets/img/edifier-logo-color.svg"
+            ></v-img>
+          </div>
+          <div class="mt-auto">
+            <v-img
+              class="mx-auto pt-15"
+              contain
+              width="250"
+              src="@/assets/img/destacado/Destacado002.jpeg"
+            ></v-img>
+            <div class="mx-15 text-center">
+              Inicia sesión para mantenerte informado de los productos y
+              servicios que tenemos para ti
+            </div>
+            <div class="py-15"></div>
+          </div>
+        </div>
+      </v-col>
+      <v-col cols="12" md="7">
+        <ValidationObserver ref="obs" v-slot="{ passes }">
+          <div class="d-flex justify-center">
+            <v-card
+              width="400"
+              flat
+              class="px-0 py-0"
+              style="
+                box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1) !important;
+                border-radius: 20px !important;
+              "
+            >
+              <div class="text-center" style="font-size: 2em; font-weight: 380">
+                Bienvenido
               </div>
-
-              <!-- <div
-                    @click="HandlerRouter('signup')"
-                    style="cursor: pointer"
-                    class="mt-2 blue--text"
-                    >Registrate
-                  </div> -->
-
-              <!-- <span
-                  @click="showRecovery = true"
-                  style="cursor: pointer; font-size: 12px"
+              <div
+                class="text-center mt-5"
+                style="
+                  font-size: 0.87em;
+                  font-weight: 400;
+                  line-height: 1.375rem;
+                "
+              >
+                Ingresa con tu cuenta
+              </div>
+              <v-col cols="12" md="12">
+                <label for="email">Correo Electrónico</label>
+                <ValidationProvider
+                  name="Nombre"
+                  rules="email|required"
+                  v-slot="{ errors }"
                 >
-                  Recuperar contraseña?
-                </span>
-                <span
+                  <v-text-field
+                    v-model="email"
+                    class="mt-2"
+                    color="#00A0E9"
+                    dense
+                    outlined
+                    placeholder="ejemplo@mail.com"
+                    :error-messages="errors"
+                  ></v-text-field>
+                </ValidationProvider>
+              </v-col>
+              <v-col cols="12" md="12" class="mt-n5">
+                <label for="email">Contraseña</label>
+                <ValidationProvider
+                  name="Contraseña"
+                  rules="required"
+                  v-slot="{ errors }"
+                >
+                  <v-text-field
+                    @keyup.enter="passes(HandlerLogin)"
+                    type="password"
+                    v-model="password"
+                    class="mt-2"
+                    color="#00A0E9"
+                    dense
+                    outlined
+                    placeholder="Ingresa tu contraseña"
+                    :error-messages="errors"
+                  ></v-text-field>
+                </ValidationProvider>
+              </v-col>
+              <v-col cols="12" md="12">
+                <v-btn
+                  :loading="loading"
+                  @click="passes(HandlerLogin)"
+                  tile
+                  elevation="0"
+                  large
+                  block
+                  dark
+                  color="#00A0E9"
+                  class="text-capitalize"
+                >
+                  Ingresar
+                </v-btn>
+                <v-btn
+                  :loading="loading"
                   @click="HandlerRouter('signup')"
-                  style="cursor: pointer; font-size: 12px"
+                  tile
+                  elevation="0"
+                  large
+                  block
+                  dark
+                  color="black"
+                  class="text-capitalize mt-1"
                 >
-                  No tienes cuenta? Registrate
-                </span> -->
-            </v-col>
-          </v-card>
-        </v-col>
-      </ValidationObserver>
+                  Registrar
+                </v-btn>
+              </v-col>
+              <v-col
+                class="text-center mt-n7 mt-md-n5"
+                cols="12"
+                sm="12"
+                md="12"
+              >
+                <div
+                  @click="HandlerRouter('login')"
+                  style="cursor: pointer"
+                  class="mt-6 blue--text"
+                >
+                  Recuperar contraseña
+                </div>
+              </v-col>
+            </v-card>
+          </div>
+        </ValidationObserver>
+      </v-col>
     </v-row>
 
     <!-- VALIDAR CUENTA -->
