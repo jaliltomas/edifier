4eaf743a8281b56e73f7f24bff623c4a3ecd8ba9
argelismoreno <argelismorenovillegas@gmail.com>
2022-06-27 15:40:57 -0400

    ajuste de validate umbral store sin stock

--- src/utils/validateUmbral.js
+++ src/utils/validateUmbral.js
@@ -1,56 +1,95 @@
-export const isValidUmbral = function ({ zipCode, dataProduct }) {
-    const userZipCode = parseInt(zipCode);
-    const productWarehouse = dataProduct.product?.product_warehouse;
-    const threshold = dataProduct.threshold;
+export const isValidUmbral = function({ zipCode, dataProduct }) {
+  if (dataProduct.store.out_stock == true) {
+    return 0;
+  }
+  const userZipCode = parseInt(zipCode);
+  const productWarehouse = dataProduct.product?.product_warehouse;
+  const threshold = dataProduct.threshold;
 
-    if (!productWarehouse.length) {
-        return 0;
-    }
+  if (!productWarehouse.length) {
+    return 0;
+  }
 
-    if (userZipCode === 2000) {
-        const warehouse = userFindWarehouse({ cp_central: 5, cp_regional: 10, productWarehouse });
-        return validateStock({ warehouse, threshold, cp_regional: 10, productWarehouse });
-    }
+  if (userZipCode === 2000) {
+    const warehouse = userFindWarehouse({
+      cp_central: 5,
+      cp_regional: 10,
+      productWarehouse
+    });
+    return validateStock({
+      warehouse,
+      threshold,
+      cp_regional: 10,
+      productWarehouse
+    });
+  }
 
-    if (userZipCode === 5000) {
-        const warehouse = userFindWarehouse({ cp_central: 5, cp_regional: 3, productWarehouse });
-        return validateStock({ warehouse, threshold, cp_regional: 3, productWarehouse });
-    }
+  if (userZipCode === 5000) {
+    const warehouse = userFindWarehouse({
+      cp_central: 5,
+      cp_regional: 3,
+      productWarehouse
+    });
+    return validateStock({
+      warehouse,
+      threshold,
+      cp_regional: 3,
+      productWarehouse
+    });
+  }
 
-    return defaultStock({ productWarehouse, threshold });
+  return defaultStock({ productWarehouse, threshold });
 };
 
-const userFindWarehouse = function ({ cp_central, cp_regional, productWarehouse }) {
-    return productWarehouse.filter(
-        (whr) =>
-            (whr.warehouse_id === cp_regional && whr.current_stock > 0) ||
-            (whr.warehouse_id === cp_central && whr.current_stock > 0)
+const userFindWarehouse = function({
+  cp_central,
+  cp_regional,
+  productWarehouse
+}) {
+  return productWarehouse.filter(
+    whr =>
+      (whr.warehouse_id === cp_regional && whr.current_stock > 0) ||
+      (whr.warehouse_id === cp_central && whr.current_stock > 0)
+  );
+};
+
+const validateStock = function({
+  warehouse,
+  threshold,
+  cp_regional,
+  productWarehouse
+}) {
+  if (warehouse.length > 1) {
+    const userWarehouse = warehouse.find(
+      whr => whr.warehouse_id == cp_regional
     );
-}
 
-const validateStock = function ({ warehouse, threshold, cp_regional, productWarehouse }) {
-    if (warehouse.length > 1) {
-        const userWarehouse = warehouse.find((whr) => whr.warehouse_id == cp_regional);
+    if (userWarehouse?.current_stock > threshold)
+      return userWarehouse.current_stock - threshold;
+    return defaultStock({ productWarehouse, threshold });
+  }
 
-        if (userWarehouse?.current_stock > threshold) return userWarehouse.current_stock - threshold;
-        return defaultStock({ productWarehouse, threshold });
-    }
+  const warehouseThreshold = warehouse.some(
+    whr => whr.current_stock > threshold
+  );
 
-    const warehouseThreshold = warehouse.some((whr) => whr.current_stock > threshold);
-
-    if (warehouseThreshold) {
-        return warehouse[0].current_stock - threshold;
-    }
-    return 0;
+  if (warehouseThreshold) {
+    return warehouse[0].current_stock - threshold;
+  }
+  return 0;
 };
 
-const defaultStock = function ({ productWarehouse, threshold }) {
-    const warehouse = productWarehouse.filter((whr) => whr.warehouse_id == 5 && whr.current_stock > 0);
+const defaultStock = function({ productWarehouse, threshold }) {
+  const warehouse = productWarehouse.filter(
+    whr => whr.warehouse_id == 5 && whr.current_stock > 0
+  );
 
-    if (warehouse.length) {
-        const warehouseThreshold = warehouse.some((whr) => whr.current_stock > threshold);
-        if (warehouseThreshold) return warehouse[0].current_stock - threshold;
-        return 0
-    }
+  if (warehouse.length) {
+    const warehouseThreshold = warehouse.some(
+      whr => whr.current_stock > threshold
+    );
+    if (warehouseThreshold) return warehouse[0].current_stock - threshold;
     return 0;
+  }
+  return 0;
 };
